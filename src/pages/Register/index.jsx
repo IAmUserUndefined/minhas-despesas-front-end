5a586edbc5c2fe3c8c141b40c4775c3bb1ad74ba
João Pedro <jpmns9103@gmail.com>
2022-01-07 21:08:26 -0300

    criar página de registro de usuário

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Header from "../../components/Header/index"
+import PagesContainer from "../../components/PagesContainer/index";
+import Form from "../../styles/form";
+import TitleForm from "../../components/FormTitle/index";
+import FormInput from "../../components/FormInput/index";
+import FormLink from "../../components/FormLink/index";
+import Button from "../../components/Button/index";
+
+const Register = () => {
+  
+    return (
+      <>
+        <Header />
+        <PagesContainer>
+          <Form name="register">
+            
+            <TitleForm>
+              Cadastro
+            </TitleForm>
+
+            <FormInput type="email" name="email" placeholder="Email" />
+            <FormInput type="password" name="password" placeholder="Senha" />
+            <FormInput type="password" name="passwordConfirm" placeholder="Confirmação de Senha" />
+  
+            <Button>
+              Cadastrar
+            </Button>
+  
+            <FormLink link="/">Já tem um cadastro?</FormLink>
+          </Form>
+        </PagesContainer>
+      </>
+    );
+  };
+  
+export default Register;
