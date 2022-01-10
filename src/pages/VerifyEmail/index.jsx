8c48b0f37996ac8054bf4012151f127797e5359c
João Pedro <jpmns9103@gmail.com>
2022-01-10 19:10:58 -0300

    criar função de verificação de email

--- src/pages/VerifyEmail/index.jsx
+++ src/pages/VerifyEmail/index.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
 
 import VerifyEmailTitle from "../../components/VerifyEmailTitle/index";
 
+import api from "../../services/api";
+
+import { useModal } from "../../providers/ModalProvider";
+
 const VerifyEmail = () => {
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const { handleShowModal } = useModal();
+
+  useEffect(() => {
+    const handleVerifyEmail = async () => {
+      await api
+        .post(`/verify-email${search}`)
+        .then(({ data }) => handleShowModal(data.response))
+        .catch(({ response }) =>
+          response
+            ? handleShowModal(response.data.response)
+            : handleShowModal("Erro no Servidor")
+        );
+    };
+    handleVerifyEmail();
+    navigate("/");
+  });
 
     return ( 
         <>
