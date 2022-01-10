236ed8245898e61f83f082086052fd4d51417b8b
João Pedro <jpmns9103@gmail.com>
2022-01-10 19:57:29 -0300

    criar função de registro de despesa

--- src/pages/RegisterExpense/index.jsx
+++ src/pages/RegisterExpense/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from "../../components/Header";
 import Aside from "../../components/Aside";
@@ -6,8 +6,53 @@ import ContainerMain from "../../components/ContainerMain";
 import Form from "../../styles/form";
 import FormInput from "../../components/FormInput/index";
 import Button from "../../components/Button/index";
+import LoadingGif from "../../components/LoadingGif/index";
+
+import api from "../../services/api";
+
+import { useModal } from "../../providers/ModalProvider";
 
 const RegisterExpense = () => {
+    const { handleShowModal } = useModal();
+    const [buttonChildren, setButtonChildren] = useState("Cadastrar Despesa");
+  
+    const handleRegisterExpense = async () => {
+      setButtonChildren(<LoadingGif />);
+  
+      const form = document.forms.expense;
+  
+      let { expenseName, dueDate, price } = form;
+  
+      if (!expenseName.value || !dueDate.value || !price.value) {
+        setButtonChildren("Cadastrar Despesa");
+        return handleShowModal("Preencha todos os campos");
+      }
+  
+      await api
+        .post("/expenses", {
+          expenseName: expenseName.value,
+          dueDate: dueDate.value,
+          price: price.value,
+        })
+        .then(({ data }) => {
+          expenseName.value = "";
+          dueDate.value = "";
+          price.value = "";
+          handleShowModal(data.response);
+        })
+        .catch(({ response }) =>
+          response
+            ? handleShowModal(response.data.response)
+            : handleShowModal("Erro no Servidor")
+        );
+  
+      expenseName.value = "";
+      dueDate.value = "";
+      price.value = "";
+  
+      setButtonChildren("Cadastrar Despesa");
+    };
+
     return ( 
         <>
             <Header />
@@ -19,7 +64,7 @@ const RegisterExpense = () => {
                     <FormInput type="date" placeholder="Data de Vencimento" name="dueDate" />
                     <FormInput type="number" placeholder="Preço" min="0" name="price" />
 
-                    <Button>Cadastrar Despesa</Button>
+                    <Button onClick={() => handleRegisterExpense()}>{buttonChildren}</Button>
 
                 </Form>
             </ContainerMain>
