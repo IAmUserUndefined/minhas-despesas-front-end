bcafa290ec16e8ac8993b320dd68a77624f755ca
João Pedro <jpmns9103@gmail.com>
2022-01-10 19:37:05 -0300

    criar funções de recuperação de senha

--- src/pages/ForgetPassword/index.jsx
+++ src/pages/ForgetPassword/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import PagesContainer from "../../components/PagesContainer/index";
 import Form from "../../styles/form";
@@ -6,8 +6,54 @@ import TitleForm from "../../components/FormTitle/index";
 import FormInput from "../../components/FormInput/index";
 import FormLink from "../../components/FormLink/index";
 import Button from "../../components/Button/index";
+import LoadingGif from "../../components/LoadingGif/index";
+
+import api from "../../services/api";
+
+import isEmailValid from "../../utils/isEmailValid";
+
+import { useModal } from "../../providers/ModalProvider";
 
 const ForgetPassword = () => {
+
+  const [buttonChildren, setButtonChildren] = useState("Enviar Email");
+  const { handleShowModal } = useModal();
+
+  const handleForgetPassword = async () => {
+    setButtonChildren(<LoadingGif />);
+
+    const form = document.forms.forgetPassword;
+
+    let { email } = form;
+
+    if (!email.value) {
+      setButtonChildren("Enviar Email");
+      return handleShowModal("Preencha o campo de email");
+    }
+
+    if (!isEmailValid(email.value)) {
+      setButtonChildren("Enviar Email");
+      email.value = "";
+      return handleShowModal("Coloque um email válido");
+    }
+
+    await api
+      .post("/user/password/send-token-password-recover", {
+        email: email.value,
+      })
+      .then(({ data }) => {
+        handleShowModal(data.response);
+      })
+      .catch(({ response }) =>
+        response
+          ? handleShowModal(response.data.response)
+          : handleShowModal("Erro no Servidor")
+      );
+
+    email.value = "";
+
+    setButtonChildren("Enviar Email");
+  };
   
     return (
       <>
@@ -19,8 +65,8 @@ const ForgetPassword = () => {
   
             <FormInput type="email" name="email" placeholder="Email" />
   
-            <Button>
-              Enviar
+            <Button onClick={() => handleForgetPassword()}>
+              {buttonChildren}
             </Button>
   
             <FormLink link="/">Lembrou sua senha?</FormLink>
--- src/pages/RecoverPassword/index.jsx
+++ src/pages/RecoverPassword/index.jsx
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import PagesContainer from "../../components/PagesContainer/index";
 import Form from "../../styles/form";
 import TitleForm from "../../components/FormTitle/index";
 import FormInput from "../../components/FormInput/index";
 import Button from "../../components/Button/index";
+import LoadingGif from "../../components/LoadingGif/index";
+
+import api from "../../services/api";
+
+import isPasswordValid from "../../utils/isPasswordValid";
+
+import { useModal } from "../../providers/ModalProvider";
 
 const RecoverPassword = () => {
+
+  const { search } = useLocation();
+  const navigate = useNavigate();
+  const handleLink = (link) => navigate(link);
+  const { handleShowModal } = useModal();
+  const [buttonChildren, setButtonChildren] = useState("Atualizar Senha");
+  console.log(search);
+
+  const handleRecoverPassword = async () => {
+    setButtonChildren(<LoadingGif />);
+
+    const form = document.forms.recoverPassword;
+
+    let { password, passwordConfirm } = form;
+
+    if (!password.value || !passwordConfirm.value) {
+      setButtonChildren("Atualizar Senha");
+      return handleShowModal("Preencha todos os campos");
+    }
+
+    const { result, message } = isPasswordValid(password.value);
+
+    if (!result) {
+      password.value = "";
+      passwordConfirm.value = "";
+      return handleShowModal(message);
+    }
+
+    await api
+      .patch(`/user/password/password-recover${search}`, {
+        password: password.value,
+        passwordConfirm: passwordConfirm.value,
+      })
+      .then(({ data }) => {
+        handleShowModal(data.response);
+        handleLink("/");
+      })
+      .catch(({ response }) =>
+        response
+          ? handleShowModal(response.data.response)
+          : handleShowModal("Erro no Servidor")
+      );
+
+    password.value = "";
+    passwordConfirm.value = "";
+
+    setButtonChildren("Atualizar Senha");
+  };
   
     return (
       <>
@@ -29,8 +85,8 @@ const RecoverPassword = () => {
                 name="passwordConfirm"
               />
   
-            <Button>
-              Atualizar Senha
+            <Button onClick={() => handleRecoverPassword()}>
+              {buttonChildren}
             </Button>
   
           </Form>
